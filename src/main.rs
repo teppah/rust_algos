59daf56bd8ed5b9bd7450d8c8f0298dd78879753
Yi Feng Yang <yifeng@yfyang.dev>
2020-08-18 15:08:12 -0400

    Add 3 D&C algos

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-fn main() {
-    println!("Hello, world!");
-    let mut nums = vec![5, 23, 6, 435, 234, 234, 5654];
+use std::ops::Add;
 
-    let found_index = binary_search(&nums, &5654).unwrap();
+fn main() {
+    let mut nums = vec![5, 23, 6, 45535, 234, 234, 5654];
+
+    selection_sort(&mut nums);
+    println!("{:?}", nums);
+
+    let found_index = binary_search(&nums, &234).unwrap();
     println!("found at index {}", found_index);
 
     let empty = binary_search(&nums, &21);
     println!("empty: {}", empty.is_none());
-    selection_sort(&mut nums);
-    println!("{:?}", nums);
+
+    let sum = sum_dc(&nums);
+    println!("sum is {}", sum);
+
+    let count = count_dc(&nums);
+    println!("count is {}", count);
+
+    let largest = max_dc(&nums);
+    println!("max is {}", largest);
 }
 
 fn binary_search<T: PartialEq + PartialOrd>(vec: &Vec<T>, item: &T) -> Option<usize> {
@@ -28,14 +39,13 @@ fn binary_search<T: PartialEq + PartialOrd>(vec: &Vec<T>, item: &T) -> Option<us
             low = mid + 1;
         }
         mid = (low + high) / 2;
-        println!("low: {}, high: {}, mid: {}", low, high, mid);
     }
     None
 }
 
 fn selection_sort<T: PartialOrd>(vec: &mut [T]) {
     for i in 0..vec.len() {
-        let (smallest, index) = smallest(&vec[i..vec.len()]);
+        let (_, index) = smallest(&vec[i..vec.len()]);
         vec.swap(i, index + i);
     }
 }
@@ -43,12 +53,37 @@ fn selection_sort<T: PartialOrd>(vec: &mut [T]) {
 fn smallest<T: PartialOrd>(vec: &[T]) -> (&T, usize) {
     let mut smallest = vec.get(0).unwrap();
     let mut index: usize = 0;
-    for i in 0..vec.len() {
-        let current = vec.get(i).unwrap();
+    for (i, current) in vec.iter().enumerate() {
         if current < smallest {
             smallest = current;
             index = i;
         }
     }
     (smallest, index)
+}
+
+// watch out for Add impl with generics
+fn sum_dc<T: Add<Output=T> + Copy>(values: &[T]) -> T {
+    return if values.len() == 1 {
+        values[0]
+    } else {
+        values[0] + sum_dc(&values[1..])
+    };
+}
+
+fn count_dc<T>(values: &[T]) -> usize {
+    return if values.len() == 1 {
+        1
+    } else {
+        1 + count_dc(&values[1..])
+    };
+}
+
+fn max_dc<T: PartialOrd + Copy>(values: &[T]) -> &T {
+    if values.len() == 1 {
+        return &values[0];
+    }
+    let current = &values[0];
+    let next = max_dc(&values[1..]);
+    return if current > next { current } else { next };
 }
